5f626ccce34fac3cf705e694b23f2a00b83aa46c
LiTing <tinating610@163.com>
2018-01-25 14:17:58 +0800

    Try_except使用
    异常处理test

---
+++ Try_except.py
@@ -0,0 +1,40 @@
+import os
+
+# os.getcwd()
+
+try:
+    file = open('Test.txt')
+    test = open('/annoroad/data1/bioinfo/PROJECT/Commercial/Medical/Leukemia/data/test.txt', 'w')
+    for line in file:
+        try:
+            (info, site) = line.split(':', 1)
+            print('prognosis:', end='')
+            print(info, end='')
+            print('site infomation:', end='')
+            print(site, end='')
+            print(site, file=test)
+        except ValueError:
+            pass
+            # except Exception as err:   ###for all error type
+            # print('pass'+str(err))
+        # file.close()
+        # test.close()
+except IOError as err:
+    print('The data file is missing!' + str(err))
+finally:
+    if 'test' in locals():
+        test.close()
+        file.close()
+        test.close()
+
+'''
+try:
+    with open('Test.txt') as data:
+        print("it's ",data.readline())
+        # print("it's ",file=data)
+except IOError as err:
+    print('File error:'+str(err))
+meters=[1,2,3]
+sec=[m*60 for m in meters]
+print(sec)
+'''
