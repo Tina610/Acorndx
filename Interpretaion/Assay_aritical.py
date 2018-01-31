49077a97fff1a22707b1a8c12da05738f1ba963c
Liting <tingli@acorndx.com>
2018-01-31 10:49:39 +0800

    no commit

---
+++ Interpretaion/Assay_aritical.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/29 16:51
+# @Author  : liting
+# @Mail    : tingli@annoroad.com
+# @File    : Assay_aritical.py
+# @Software: PyCharm
+
+import os, sys
+import re
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
