588261eb8d78ca4f85b3da525183663c7d3cb554
Liting <tingli@acorndx.com>
2018-02-04 01:05:36 +0800

    用qsge 分线程跑，分号分割

--- dataliy/QHB_adapter.py
+++ dataliy/QHB_adapter.py
@@ -22,13 +22,13 @@ def getfast(indir,outdir):
         r1fq='{0}/{1}_R1.fq.gz'.format(dirroad,dir)
         r2fq='{0}/{1}_R2.fq.gz'.format(dirroad,dir)
         if os.path.isfile(r1fq):
-            Qsub.write('/annoroad/share/software/install/cutadapt-1.2.1/bin/cutadapt -a AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA -O 5  --info-file={dirroad}_R1.adapter.txt {fqgz} 1>/dev/null 2>/dev/null &&  gzip {dirroad}_R1.adapter.gz\n'
+            Qsub.write('/annoroad/share/software/install/cutadapt-1.2.1/bin/cutadapt -a AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA -O 5  --info-file={dirroad}_R1.adapter.txt {fqgz} 1>/dev/null 2>/dev/null &&  gzip {dirroad}_R1.adapter.gz;\n'
                        .format(dirroad=dirroad,fqgz=r1fq))
         if os.path.isfile(r2fq):
             Qsub.write('/annoroad/share/software/install/cutadapt-1.2.1/bin/cutadapt -a AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA -O 5  --info-file={dirroad}_R2.adapter.txt {fqgz} 1>/dev/null 2>/dev/null &&  gzip {dirroad}_R2.adapter.gz\n'
                 .format(dirroad=dirroad, fqgz=r2fq))
         Qsub.write('cp {0} {1}/'.format(r1fq,dirroad))
-        Qsub.write('cp {0} {1}/'.format(r2fq,dirroad))
+        Qsub.write('cp {0} {1};/'.format(r2fq,dirroad))
     Qsh.write('/annoroad/share/software/install/Python-3.3.2/bin/python3 /annoroad/data1/bioinfo/PROJECT/Commercial/Medical/Leukemia/bin/Leu_V3/Leu_V3.0.1_beta subscript/qsub_sge.py -m 30 -q cancer.q -re 'vf=10G' -nodu {0}'
     .format(Qsub))
 
